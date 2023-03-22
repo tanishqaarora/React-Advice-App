171f2b02a52a97ecda14d50d72c5d336d59af6a5
Tanishqa Arora <tanishqa164@gmail.com>
2023-03-22 22:56:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component {
+    state = { advive: '' };
+
+    componentDidMount() {
+        
+    }
+
+
+
+    render() {
+        return (
+            <h1>APP</h1>
+        )
+    }
+}
+
+export default App;
